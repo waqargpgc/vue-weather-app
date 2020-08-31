2cdaaecacb16bb50de28ff11c819a3e64701ba2f
WAQAR AHMAD <waqargpgc@gmail.com>
2020-08-31 09:35:20 -0600

    second commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <Weather msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import Weather from './components/Weather.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Weather
+  }
+}
+</script>
+
+<style>
+body {
+  margin: 0px !important;
+}
+</style>
